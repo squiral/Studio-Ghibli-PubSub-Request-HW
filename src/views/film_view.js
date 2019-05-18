9a4b46032b8e03ffc97ef58ab269aa60b0a269ae
Alison Williams <alison.lauriewill@gmail.com>
2019-05-18 16:21:50 +0100

    Director and release date elements added and more CSS styling

--- src/views/film_view.js
+++ src/views/film_view.js
@@ -7,9 +7,20 @@ FilmView.prototype.createFilmDetailViewElement = function () {
   const filmDetailViewElement = document.createElement('div');
   filmDetailViewElement.classList.add('film');
 
+  const filmHeaderDiv = this.createHeaderDiv();
+  filmDetailViewElement.appendChild(filmHeaderDiv)
 
   const filmHeadingElement = this.createFilmHeading();
-  filmDetailViewElement.appendChild(filmHeadingElement);
+  filmHeaderDiv.appendChild(filmHeadingElement);
+
+  const releaseDate = this.createReleaseDateElement();
+  filmHeaderDiv.appendChild(releaseDate);
+
+  const filmDirectorElement = this.createFilmDirectorElement();
+  filmDetailViewElement.appendChild(filmDirectorElement);
+
+  const hrElement = this.createHrElement();
+  filmDetailViewElement.appendChild(hrElement);
 
   const filmSummaryParagraph = this.createFilmSummaryParagraph();
   filmDetailViewElement.appendChild(filmSummaryParagraph);
@@ -18,11 +29,36 @@ FilmView.prototype.createFilmDetailViewElement = function () {
 
 };
 
+FilmView.prototype.createHeaderDiv = function () {
+  const headerDiv = document.createElement('div');
+  headerDiv.classList.add('film-header-div');
+  return headerDiv;
+};
+
 FilmView.prototype.createFilmHeading = function () {
-  const filmHeader = document.createElement('h2');
+  const filmHeader = document.createElement('h3');
   filmHeader.classList.add('film-name');
   filmHeader.textContent = this.film.title
   return filmHeader;
+}
+
+FilmView.prototype.createFilmDirectorElement = function () {
+  const directorElement = document.createElement('h4');
+  directorElement.classList.add('director');
+  directorElement.textContent = this.film.director
+  return directorElement;
+};
+
+FilmView.prototype.createHrElement = function () {
+  const hrElement = document.createElement('hr');
+  return hrElement;
+}
+
+FilmView.prototype.createReleaseDateElement = function () {
+  const releaseDate = document.createElement('h4');
+  releaseDate.classList.add('release-date');
+  releaseDate.textContent = this.film.release_date;
+  return releaseDate;
 }
 
 FilmView.prototype.createFilmSummaryParagraph = function () {
